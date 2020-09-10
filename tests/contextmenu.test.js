6ff144115c5c6c41cf24b321803c21b3dcf16ae7
Martin <marb61a@gmail.com>
2020-09-10 20:17:27 +0100

    Updating keypress and contextmenu suites with tests

--- tests/contextmenu.test.js
+++ tests/contextmenu.test.js
@@ -9,7 +9,21 @@ describe('Context Menu Page tests', () => {
     });
 
     it('should visit context-menu page', async() => {
+        await page.on('displayMessage', async ()=> {
+            await displayMessage.dismiss();
+        });
+
         await contextMenuPage.visit();
+    });
+
+    it('should generate a JS alert', async() => {
+        await page.waitForSelector('#hot-spot');
+
+        // Using the right hand button to click
+        await page.click('#hot-spot', {
+            button: 'right'
+        });
+    
     });
 
     it('should generate screenshot', async () => {
